a2aec0d85a04e86a6b0080c2b71260f5eda22806
JaewooShin <chung_103@naver.com>
2022-09-06 10:48:20 +0900

    new_flutter 2022_09_06

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,17 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:new_flutter/src/components/image_data.dart';
+import 'package:new_flutter/src/controller/bottom_nav_controller.dart';
 
-class App extends StatelessWidget {
+class App extends GetView<BottomNavController> {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-        child: Scaffold(
-          appBar: AppBar(),
-          body: Container(),
+      child: Scaffold(
+        appBar: AppBar(),
+        body: Container(),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          currentIndex: 0,
+          elevation: 0,
+          onTap: (value) {},
+          items: [
+            BottomNavigationBarItem(
+              icon: ImageData(IconsPath.homeOff),
+              activeIcon: ImageData(IconsPath.homeOn),
+              label: 'home',
+            ),
+            BottomNavigationBarItem(
+              icon: ImageData(IconsPath.searchOff),
+              activeIcon: ImageData(IconsPath.searchOn),
+              label: 'home',
+            ),
+            BottomNavigationBarItem(
+              icon: ImageData(IconsPath.uploadIcon),
+              label: 'home',
+            ),
+            BottomNavigationBarItem(
+              icon: ImageData(IconsPath.activeOff),
+              activeIcon: ImageData(IconsPath.activeOn),
+              label: 'home',
+            ),
+            BottomNavigationBarItem(
+              icon: Container(
+                width: 30,
+                height: 30,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.grey,
+                ),
+              ),
+              label: 'home',
+            ),
+          ],
         ),
-        onWillPop: () async {
-          return false;
-        });
+      ),
+      onWillPop: () async {
+        return false;
+      },
+    );
   }
 }
---
+++ lib/src/controller/bottom_nav_controller.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+
+class BottomNavController extends GetxController{
+  RxInt pageIndex = 0.obs;
+
+
+
+}
